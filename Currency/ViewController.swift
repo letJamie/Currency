ec4d3928c6fddfe3ffc076a12e0d15837812d69e
letJamie <stillbut@naver.com>
2020-09-30 00:43:05 +0900

    Create urlSession

--- Currency/ViewController.swift
+++ Currency/ViewController.swift
@@ -12,15 +12,10 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var cadLabel: UILabel!
-    
     @IBOutlet weak var chfLabel: UILabel!
-    
     @IBOutlet weak var gbpLabel: UILabel!
-    
     @IBOutlet weak var jpyLabel: UILabel!
-    
     @IBOutlet weak var usdLabel: UILabel!
-    
     @IBOutlet weak var tryLabel: UILabel!
     
     override func viewDidLoad() {
@@ -29,6 +24,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getRatesClicked(_ sender: Any) {
+        
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=422be1d35ae329b2e6bc148828387a5a&format=1")
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            
+            if error != nil {
+                
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                
+                let OKButton = UIAlertAction(title: : "OK", style: UIAlertAction.Style.default, handler: nil)
+                
+                alert.addAction(OKButton)
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+            else {
+                
+                
+            }
+        }
     }
     
 }
